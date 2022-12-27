9821962584c1fea3a82c2cc386e332af788282d7
bystrovpavelgit <bystrov.pavelme@gmail.com>
2022-12-27 17:27:58 +0300

    created startup script

---
+++ run_webapp.sh
@@ -0,0 +1,4 @@
+# Apache License 2.0 Copyright (c) 2022 Pavel Bystrov
+# shell script to start web-app
+export FLASK_APP=webapp && export FLASK_ENV=development && export FLASK_DEBUG=1 && flask run
+
