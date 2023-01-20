41b98a3d36beeb073d7e8d9af5bcf580596325d3
bystrovpavelgit <bystrov.pavelme@gmail.com>
2023-01-20 20:14:21 +0300

    new endpoint for exp smoothing

--- webapp/stat/views.py
+++ webapp/stat/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from webapp.utils.enrich_sunspots import get_enriched_dataframe, \
     get_results_for_best_classifier
+from webapp.utils.trends_util import exponential_smoothing, \
+    double_exponential_smoothing
 
 blueprint = Blueprint("stat", __name__, url_prefix="/stat")
 
@@ -16,7 +18,24 @@ def rolling_mean():
     y3 = df["mean_12y"].values.tolist()
     return render_template("stat/charts.html",
                            info=info,
-                           time=time[:period],
+                           time=time,
+                           y=y1[:period],
+                           y2=y2[:period],
+                           y3=y3[:period])
+
+
+@blueprint.route("/exp_smooth")
+def rolling_mean():
+    df = get_enriched_dataframe()
+    info = {'graph': 'Moving average for sunspots numbers'}
+    time = df["year_float"].values.tolist()
+    period = len(time)
+    y1 = df["sunspots"].values.tolist()
+    y2 = exponential_smoothing(df["sunspots"], .9).values.tolist()
+    y3 = double_exponential_smoothing(df["sunspots"], .9, .9).values.tolist()
+    return render_template("stat/charts.html",
+                           info=info,
+                           time=time,
                            y=y1[:period],
                            y2=y2[:period],
                            y3=y3[:period])
