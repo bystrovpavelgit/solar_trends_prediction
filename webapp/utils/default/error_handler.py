68c61e1e31658921813ebfd784c462e93185773f
bystrovpavelgit <bystrov.pavelme@gmail.com>
2023-04-03 17:54:29 +0300

    added handler

--- webapp/utils/default/error_handler.py
+++ webapp/utils/default/error_handler.py
@@ -2,14 +2,15 @@
     Apache License 2.0 Copyright (c) 2023 Pavel Bystrov
     error handler functions
 """
-from werkzeug.exceptions import HTTPException
 from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.exceptions import HTTPException
 
 
-def handler_exception(err: Exception):
+def exception_handler(err: Exception):
     """ common exception handler"""
     res = ({'message': type(err).__qualname__.lower(),
-            'detail': str(err)}, 500, {'Content-Type': 'application/json'})
+            'detail': str(err)}, 500, {'Content-Type': 'application/json'}
+           )
     return res
 
 
@@ -23,14 +24,14 @@ def handler_http_exception(err: HTTPException):
 
 def handler_input_value_exception(err: ValueError):
     """ handle input_value_exception """
-    return handler_exception(err)
+    return exception_handler(err)
 
 
 def handler_file_not_found_exception(err: FileNotFoundError):
     """ handle file_not_found_exception """
-    return handler_exception(err)
+    return exception_handler(err)
 
 
 def handler_sql_exception(err: SQLAlchemyError):
     """ handle sql_exception """
-    return handler_exception(err)
+    return exception_handler(err)
